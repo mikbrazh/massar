40365a456deb80b9af3a2fd06cfdb90db1c0580e
Mikhail Brazhnik <mikbrazh@gmail.com>
2023-10-11 15:34:28 +0300

    done

--- src/js/main.js
+++ src/js/main.js
@@ -119,17 +119,17 @@ if ( document.querySelector('.modal') ) {
 /* ======= modal END ======= */
 
 /* ======= preloader START ======= */
-let hidePreloader = function() {
-	setTimeout(function() {
-	  let preloader = document.getElementById('preloader');
+// let hidePreloader = function() {
+// 	setTimeout(function() {
+// 	  let preloader = document.getElementById('preloader');
 	
-	  if (!preloader.classList.contains('preloader--hidden')) {
-		  preloader.classList.add('preloader--hidden');
-	  }
-	}, 300)
-  };
+// 	  if (!preloader.classList.contains('preloader--hidden')) {
+// 		  preloader.classList.add('preloader--hidden');
+// 	  }
+// 	}, 300)
+//   };
   
-  document.addEventListener('DOMContentLoaded', hidePreloader());
+//   document.addEventListener('DOMContentLoaded', hidePreloader());
 /* ======= preloader END ======= */
 
 /* ======= Canvi mobile navbar START ======= */
